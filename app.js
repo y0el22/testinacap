9b92e698e8de41311f80e57b7cd9aa7591183ae4
Manuel  Heyers <manuel.heyers@gmail.com>
2019-06-06 15:22:34 -0400

    Revert "agregar archivo uno.js"
    This reverts commit 0854f0d6fedfc7942a64bef3bb85c9aef41e35ee.

--- app.js
+++
@@ -1,3 +0,0 @@
-console.log("primer archivo");
-console.log("Otra linea!! ");
-ASFSAFKASKSKF
--- dos.js
+++
@@ -1,3 +0,0 @@
-console.log("Modificacion aqui");
-console.log("ROMPIMOS EL CODIGO!!")
-FSAAFSJASFJSJFASSJSAFJAFJ
